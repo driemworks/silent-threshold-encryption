8d095bb6d6f5eb7b611b97c1f5e640b229d0cf0d
driemworks <driemworks@idealabs.network>
2025-08-22 22:51:54 -0500

    update gitignore

--- masked-result/target/debug/deps/masked_result-b0c3ecaaac472dd6.d
+++
@@ -1,5 +0,0 @@
-/home/driemworks/ideal/silent-threshold-encryption/masked-result/target/debug/deps/libmasked_result-b0c3ecaaac472dd6.so: src/lib.rs
-
-/home/driemworks/ideal/silent-threshold-encryption/masked-result/target/debug/deps/masked_result-b0c3ecaaac472dd6.d: src/lib.rs
-
-src/lib.rs:
